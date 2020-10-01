64ef353b7ee42d274ff680206a496396fd9d2336
Demis Marcellino <d.marcellino.1032@vallauri.edu>
2020-10-01 13:19:54 +0200

    second commit

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -1,12 +1,121 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FormulaOneConsole
 {
     class Program
     {
+        public const string WORKINGPATH = @"C:\data\formulaone\";
+        private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("*** FORMULA ONE -  BATCH OPERATIONS ***");
+            char scelta = ' ';
+            do
+            {
+                Console.WriteLine("\n*** FORMULA ONE - CONSOLE ***\n");
+                Console.WriteLine("1 - Create Countries");
+                Console.WriteLine("2 - Create Teams");
+                Console.WriteLine("3 - Create Drivers");
+                Console.WriteLine("------------------");
+                Console.WriteLine("X - EXIT\n");
+                scelta = Console.ReadKey(true).KeyChar;
+                switch (scelta)
+                {
+                    case '1':
+                        ExecuteSqlScript("Countries.sql");
+                        break;
+                    case '2':
+                        ExecuteSqlScript("Teams.sql");
+                        break;
+                    case '3':
+                        ExecuteSqlScript("Drivers.sql");
+                        break;
+                    default:
+                        if (scelta != 'X' && scelta != 'x') Console.WriteLine("\nUncorrect Choice - Try Again\n");
+                        break;
+                }
+            } while (scelta != 'X' && scelta != 'x');
+        }
+
+        public static void ExecuteSqlScript(string sqlScriptName)
+        {
+            var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
+            fileContent = fileContent.Replace("\r\n", "");
+            fileContent = fileContent.Replace("\r", "");
+            fileContent = fileContent.Replace("\n", "");
+            fileContent = fileContent.Replace("\t", "");
+            var sqlqueries = fileContent.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var con = new SqlConnection(CONNECTION_STRING);
+            var cmd = new SqlCommand("query", con);
+            con.Open(); int i = 0;
+            foreach (var query in sqlqueries)
+            {
+                cmd.CommandText = query; i++;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException err)
+                {
+                    Console.WriteLine("Errore in esecuzione della query numero: " + i);
+                    Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
+                }
+            }
+            con.Close();
+        }
+
+        public static void DropTable(string tableName)
+        {
+            var con = new SqlConnection(CONNECTION_STRING);
+            var cmd = new SqlCommand("Drop Table " + tableName + ";", con);
+            con.Open();
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException err)
+            {
+                Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
+            }
+            con.Close();
+        }
+
+        static bool callExecuteSqlScript(string scriptName)
+        {
+            try
+            {
+                ExecuteSqlScript(scriptName + ".sql");
+                Console.WriteLine("\nCreate " + scriptName + " - SUCCESS\n");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nCreate " + scriptName + " - ERROR: " + ex.Message + "\n");
+                return false;
+            }
+        }
+
+
+        static bool callDropTable(string tableName)
+        {
+            try
+            {
+                DropTable(tableName);
+                Console.WriteLine("\nDROP " + tableName + " - SUCCESS\n");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nDROP " + tableName + " - ERROR: " + ex.Message + "\n");
+                return false;
+            }
         }
     }
 }
